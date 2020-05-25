6897f099e52bac5b3db4145f23bdd729e21f12b7
Zahra Fazel <zfazel1379@gmail.com>
2020-05-25 21:49:12 +0430

    Custom Button

--- app/src/main/java/com/example/slide2/MainActivity.java
+++ app/src/main/java/com/example/slide2/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.slide2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_custom_button_example);
+    }
+
+
+    //For activity_custom_button_example
+    public void sendMessage(View view)
+    {
+        Toast.makeText(MainActivity.this, "Button Clicked", Toast.LENGTH_LONG).show();
     }
 }
